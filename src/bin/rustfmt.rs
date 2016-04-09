9be8b9b2a6595d7c595607ce043d90266099a5ec
Kamal Marhubi <kamal@marhubi.com>
2016-04-09 17:54:19 -0400

    rustfmt: Simplify match in project file lookup loop
    This commit changes the match in `lookup_project_file` to use pattern
    guards.

--- src/bin/rustfmt.rs
+++ src/bin/rustfmt.rs
@@ -65,19 +65,12 @@ fn lookup_project_file(dir: &Path) -> Result<Option<PathBuf>> {
     loop {
         let config_file = current.join("rustfmt.toml");
         match fs::metadata(&config_file) {
-            Ok(md) => {
-                // Properly handle unlikely situation of a directory named `rustfmt.toml`.
-                if md.is_file() {
-                    return Ok(Some(config_file));
-                }
-            }
-            // If it's not found, we continue searching; otherwise something went wrong and we
-            // return the error.
-            Err(e) => {
-                if e.kind() != ErrorKind::NotFound {
-                    return Err(From::from(e));
-                }
-            }
+            // Only return if it's a file to handle the unlikely situation of a directory named
+            // `rustfmt.toml`.
+            Ok(md) if md.is_file() => return Ok(Some(config_file),
+            // Return the error if it's something other than `NotFound`; otherwise we didn't find
+            // the project file yet, and continue searching.
+            Err(e) if e.kind() != ErrorKind::NotFound => return Err(From::from(e)),
         }
 
         // If the current directory has no parent, we're done searching.
